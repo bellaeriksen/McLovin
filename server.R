94c40fb1f3e4774bb443af99a5f88642afa6fdf7
bellaeriksen <beriksen@uw.edu>
2019-06-01 18:17:27 -0700

    added libraries

--- server.R
+++ server.R
@@ -1,5 +1,11 @@
+library("dplyr")
+library("ggplot2")
+library("shiny")
+library("shinythemes")
+
 library("httr")
 library("readxl")
 GET("https://query.data.world/s/ymhlkbigf63uimejlhgmmwbnucwbly", write_disk(tf <- tempfile(fileext = ".xlsx")))
 df <- read_excel(tf)
-View(df)
+ #View(df)
+
---
+++ ui.R
@@ -0,0 +1,5 @@
+library("dplyr")
+library("ggplot2")
+library("shiny")
+library("shinythemes")
+
