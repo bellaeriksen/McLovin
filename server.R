b020d14a5ef7a2f39644f06d22c9ce10f9fd37dc
Roshani Shiwakoti <roshanis@uw.edu>
2019-06-04 14:13:34 -0700

    resolved conflict

--- server.R
+++ server.R
@@ -11,21 +11,6 @@ df <- read_excel(tf)
 
 server <- function(input, output) {
   output$plot <- renderPlot({
-<<<<<<< HEAD
-    data_frame <- as.data.frame(input$selectPlace)
-    data <- data_frame %>% df %>% select("Obstacle Name")
-    ##x <- df[[data]]
-    ##num <- add_count(data, "Obstacle Name")
-    ## y <- num
-    x <- unique(data)
-    y <- count(data, "Obstacle Name")
-    title <- paste0(
-      "American Ninja Warrior in", input$selectPlace, "."
-    )
-    ggplot(mapping = aes(x = x, y = num)) +
-    geom_col()
-=======
-
     # data_frame <- as.data.frame(input$selectPlace)
     # data <- data_frame %>% select("Obstacle Name")
     data <- df %>% filter(Location == input$selectPlace)
@@ -45,6 +30,5 @@ server <- function(input, output) {
      print(plot)
        
      #hist(x, breaks = y, col = 'darkgray', border = 'white')
->>>>>>> aacedcda3decfde7152a3ae202c8fb900c09f87a
   })
 }
