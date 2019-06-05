595c7a7f882d9d7df33da115695410d09b089f97
Roshani Shiwakoti <roshanis@uw.edu>
2019-06-04 22:54:07 -0700

    pushing completed project

--- server.R
+++ server.R
@@ -2,32 +2,86 @@ library("dplyr")
 library("ggplot2")
 library("shiny")
 library("shinythemes")
-
 library("httr")
 library("readxl")
+
 GET("https://query.data.world/s/ymhlkbigf63uimejlhgmmwbnucwbly", write_disk(tf <- tempfile(fileext = ".xlsx")))
 df <- read_excel(tf)
- View(df)
        
+## this will filter the count for the Locations this Obstacle/season appears in!?
 server <- function(input, output) {
-  output$plot <- renderPlot({
-    data <- df %>% select("Location", "Obstacle Name") %>% 
-            filter(`Obstacle Name` == input$selectObstacle)
-    
-    x <- unique(data$`Location`)
+  output$intro <- renderText({
+    "We worked with the American Ninja Warrior Obstacle History dataset, which we obtained
+      through a GitHub repository called “Awesome Public Datasets”. This repository led us to the 
+      dataset that we are currently working with--provided by data.world and assembled by a user. 
+      The original information was credited to sasukepedia, where the information was last updated 
+      in December of 2018. The data lists of all the obstacles from all 10 seasons of American Ninja 
+      Warrior.  
 
-    # How to count for the number of Locations this Obstacle appears in!?
-    # y <- count(data, data$`Obstacle Name` == "Obstacle Name", data$Location ==  'Location')
-    y <- data %>% () 
-        
-    title <- paste0(
-      "American Ninja Warrior in", input$selectPlace, "."
-    )
-         
-    plot <- ggplot(data, aes(x = x, y = y)) +
-      geom_col()
-    print(plot)
-       
-    #hist(x, breaks = y, col = 'darkgray', border = 'white')
+
+      Primarily, our dataset would be of interest to people who are studying to be the next American 
+      Ninja Warrior. Alternatively, it could also be used for avid fans, people who watch the show. For 
+      the purpose of our project, we have decided to target this dataset to those who have an interest in 
+      becoming the next American Ninja Warrior and would like to know about the obstacles they will have to 
+      overcome.
+
+      Three things our audience would want to learn from our data are: 
+
+      What are the possible locations that they will have to compete at? This question would help them gain 
+            insight on what weather they will be performing in and other factors that could become an obstacle 
+            on their path to becoming the next American Ninja Warrior.
+      What order will the obstacles be in during the competition, and in what round will they face which obstacles?
+            Knowing this information allows for the audience to plan and organize themselves. 
+      What type of obstacles will competitors face? By knowing more about this, the user is able to practice these 
+            obstacles, and train themselves to overcome them for the real battle.
+    
+"
+  })
+  
+  output$plot <- renderPlot({
+    filtered <- df %>%
+      rename(ObstacleName = "Obstacle Name") %>% 
+      select(Location, ObstacleName, Season) %>% 
+      rowwise() %>% 
+      filter(ObstacleName == input$selectObstacle, Season >= input$slider[1], Season <= input$slider[2])
+    plot <- ggplot(filtered, aes(x = Location, fill = Season)) +
+      geom_bar() +
+      scale_fill_continuous(low = "#132b43", high = "Red") +
+      labs(
+        title = "Locations per Season for Obstacle"
+      )
+    plot
+  })
+  
+  output$text2 <- renderText({
+    paste("This bar graph gives a visual representation of the number of localities 
+           that appeared with each obstacle and range of season in the history of 
+           American Ninja Warrior. The bar plot is scaled and is colored to reflect the seasons. 
+           From this information, the user, and potential future ninja, is able to identify which Location,
+           and Obstacle they should expect in their journey") 
+  })
+  
+  output$table <- renderTable({
+    filtered <- df %>% filter(Location == input$location, Season == input$season, `Round/Stage` == input$round)
+    out <- filtered %>% select(`Obstacle Name`, `Obstacle Order`)
+    print(out)
+  })
+  
+  output$conclusionText <- renderText({
+    paste("With the information we have have provided, you will become the next American Ninja Warrior! You are now well 
+          aware of how to prepare and train, what obstacles you might expect to come across, and in what season it is most common.
+          In the future will will provide you with more information that will help you proceed on your journey. Such as, finding new
+          data sets that include severity of obstacles, to see how challenging each round is, and the average time needed to complete. 
+          We will also expand our dataset to include latitude and longitude values that can prepare a map for a more interactive, 
+          and easy to comprehend visual interface.")
+  })
+  
+  output$text <- renderText({
+    "Are you looking for specific information about how to train for American Ninja Warrior?
+    You've come to the right place! Here is a detailed list about obstacles in the exact order 
+    they come in for a specifc season,round, and location. This provides for excellent training
+    opportunities to know exactly
+    which obstacles come after one another so that you are able to train with perfect transitions
+    after you have crushed the last obstacle."
   })
 }
--- ui.R
+++ ui.R
@@ -3,36 +3,48 @@ library("ggplot2")
 library("shiny")
 library("shinythemes")
 source("server.R")
+source("helper.R")
 
-getObstacle <- df %>% select("Obstacle Name")
-uniqueObstacle <- unique(getObstacle)
-uniqueList <- uniqueObstacle$`Obstacle Name`
-
-getCompetition <- df %>% select("Round/Stage")
-theRound <- unique(getCompetition)
-uniqueRounds <- theRound$`Round/Stage`
+rsconnect::setAccountInfo(name='roshani-shiwa', token='4A880C6FE1D688961A003EC789AD2CF2', 
+                         secret='QAZnTdSISlUkqFMyQMTX+NCQHFg7gz/c5YETedU4')
 
 ui <- navbarPage(
   "American Ninja Warrior",
   theme = shinytheme("darkly"),
-  tabPanel("Obstacles in Your Locality", fluidPage(
+  introduction <- tabPanel(
+    "Introduction", fluidPage(
+      mainPanel(
+        h5(textOutput("intro")))
+    )    
+  ),
+  tabPanel("Localities per Obstacle", fluidPage(
     sidebarLayout(
       sidebarPanel(
         selectInput("selectObstacle", label = h3("Select Obstacle"),
-                   choices = c(uniqueList), selected = uniqueList[1])
-        #selectInput("selectRound", label = h3("Select Round"),
-                    #choices = c(uniqueRounds), selected = uniqueRounds[1])
+                   choices = c(uniqueList), selected = uniqueList[1]),
+        sliderInput("slider", label = h3("Select Season"), min = seasonMin,
+                    max = seasonMax, value = c(4, 6))
       ),
-    mainPanel(plotOutput("plot"))
+      mainPanel(plotOutput("plot"), h5(textOutput("text2")))
     )
+  )),
+  tabPanel("Obstacles per Specific Round", fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          radioButtons("season", label = h3("Select Season"),
+                       choices = c(uniqueListSeason), selected = uniqueListSeason[1]),
+          selectInput("location", label = h3("Select Location"),
+                      choices = c(uniqueListLocation), selected = uniqueListLocation[1]),
+          selectInput("round", label = h3("Select Round"),
+                      choices = c(uniqueListRound), selected = uniqueListRound[1])
+        ),
+        mainPanel(tableOutput("table"),h5(textOutput("text")))
+    )
+  )),
+  tabPanel("Conclusion", fluidPage(
+    mainPanel(h5(textOutput("conclusionText")))
   ))
-  # tabPanel("Add title lols", fluidPage(
-    # sidebarLayout(
-      # sidebarPanel(
-        # selectInput()
-      # )
-    # ) 
-  # ))
 )
 
 shinyApp(ui = ui, server = server)
+rsconnect::deployApp()
