a751d19e5a693b78aac4b7c62abc53bdcbe11664
bellaeriksen <beriksen@uw.edu>
2019-06-04 23:26:18 -0700

    close #5

--- server.R
+++ server.R
@@ -7,51 +7,6 @@ library("readxl")
 
 GET("https://query.data.world/s/ymhlkbigf63uimejlhgmmwbnucwbly", write_disk(tf <- tempfile(fileext = ".xlsx")))
 df <- read_excel(tf)
-<<<<<<< HEAD
-
-server <- function(input, output) {
-  output$table <- renderTable({
-    filtered <- df %>% filter(Location == input$location, Season == input$season, `Round/Stage` == input$round)
-    out <- filtered %>% select(`Obstacle Name`, `Obstacle Order`)
-    print(out)
-  })
-  
-  output$text <- renderText({
-    "Are you looking for specific information about how to train for American Ninja Warrior?
-    You've come to the right place! Here is a detailed list about obstacles in the exact order 
-    they come in for a specifc season,round, and location. This provides for excellent training
-    opportunities to know exactly
-    which obstacles come after one another so that you are able to train with perfect transitions
-    after you have crushed the last obstacle."
-  })
-  
-  output$intro <- renderText({
-    "We worked with the American Ninja Warrior Obstacle History dataset, which we obtained
-      through a GitHub repository called “Awesome Public Datasets”. This repository led us to the 
-      dataset that we are currently working with--provided by data.world and assembled by a user. 
-      The original information was credited to sasukepedia, where the information was last updated 
-      in December of 2018. The data lists of all the obstacles from all 10 seasons of American Ninja 
-      Warrior.  
-
-
-      Primarily, our dataset would be of interest to people who are studying to be the next American 
-      Ninja Warrior. Alternatively, it could also be used for avid fans, people who watch the show. For 
-      the purpose of our project, we have decided to target this dataset to those who have an interest in 
-      becoming the next American Ninja Warrior and would like to know about the obstacles they will have to 
-      overcome.
-
-      Three things our audience would want to learn from our data are: 
-
-      What are the possible locations that they will have to compete at? This question would help them gain 
-            insight on what weather they will be performing in and other factors that could become an obstacle 
-            on their path to becoming the next American Ninja Warrior.
-      What order will the obstacles be in during the competition, and in what round will they face which obstacles?
-            Knowing this information allows for the audience to plan and organize themselves. 
-      What type of obstacles will competitors face? By knowing more about this, the user is able to practice these 
-            obstacles, and train themselves to overcome them for the real battle.
-    
-"
-=======
        
 ## this will filter the count for the Locations this Obstacle/season appears in!?
 server <- function(input, output) {
@@ -128,7 +83,6 @@ server <- function(input, output) {
     opportunities to know exactly
     which obstacles come after one another so that you are able to train with perfect transitions
     after you have crushed the last obstacle."
->>>>>>> 595c7a7f882d9d7df33da115695410d09b089f97
   })
   
 }
--- ui.R
+++ ui.R
@@ -1,67 +1,29 @@
+#load libraries
 library("dplyr")
 library("ggplot2")
 library("shiny")
 library("shinythemes")
+library("rsconnect")
 source("server.R")
 source("helper.R")
-
-<<<<<<< HEAD
-getLocation <- df %>% select("Location")
-uniqueLocation <- unique(getLocation)
-uniqueListLocation <- uniqueLocation$Location
-
-getRound <- df %>% select("Round/Stage")
-uniqueRound <- unique(getRound)
-uniqueListRound <- uniqueRound$'Round/Stage'
-
-getSeason <- df %>% select("Season")
-uniqueSeason <- unique(getSeason)
-uniqueListSeason <- uniqueSeason$Season
-  
-introduction <- tabPanel(
-  "Introduction", fluidPage(
-    mainPanel(
-      h5(textOutput("intro"))
-    )
-  )    
-)
-   page_two <- tabPanel(
-     "Obstacles per Specific Round", fluidPage(
-     sidebarLayout(
-       sidebarPanel(
-         radioButtons("season", label = h3("Select Season"),
-                      choices = c(uniqueListSeason), selected = uniqueListSeason[1]),
-         selectInput("location", label = h3("Select Location"),
-                     choices = c(uniqueListLocation), selected = uniqueListLocation[1]),
-         selectInput("round", label = h3("Select Round"),
-                       choices = c(uniqueListRound), selected = uniqueListRound[1])
-         ),
-       mainPanel(
-         tableOutput("table"),
-         h5(textOutput("text"))
-       )
-     )
-   ) 
-)
    
-=======
-rsconnect::setAccountInfo(name='roshani-shiwa', token='4A880C6FE1D688961A003EC789AD2CF2', 
-                         secret='QAZnTdSISlUkqFMyQMTX+NCQHFg7gz/c5YETedU4')
->>>>>>> 595c7a7f882d9d7df33da115695410d09b089f97
-
+rsconnect::setAccountInfo(name='bellaeriksen',
+                          token='40C645D5E3B1DE2789024044822AACC6',
+                          secret='Nu9MZEJimk1hAM4d8JQfRhaLfVPSJtLfViOXOmF1')
+#create shiny ui
 ui <- navbarPage(
   "American Ninja Warrior",
   theme = shinytheme("darkly"),
-<<<<<<< HEAD
-  introduction,
-  page_two
-=======
-  introduction <- tabPanel(
+  
+  # create 'introduction' panel
+  tabPanel(
     "Introduction", fluidPage(
       mainPanel(
         h5(textOutput("intro")))
     )    
   ),
+  
+  # create 'localities per obstacle' panel
   tabPanel("Localities per Obstacle", fluidPage(
     sidebarLayout(
       sidebarPanel(
@@ -73,6 +35,8 @@ ui <- navbarPage(
       mainPanel(plotOutput("plot"), h5(textOutput("text2")))
     )
   )),
+  
+  # create 'obstacles per specific round' panel
   tabPanel("Obstacles per Specific Round", fluidPage(
       sidebarLayout(
         sidebarPanel(
@@ -86,11 +50,13 @@ ui <- navbarPage(
         mainPanel(tableOutput("table"),h5(textOutput("text")))
     )
   )),
+  
+  #create 'conclusion' panel
   tabPanel("Conclusion", fluidPage(
     mainPanel(h5(textOutput("conclusionText")))
   ))
->>>>>>> 595c7a7f882d9d7df33da115695410d09b089f97
 )
 
+# publishes shiny online
 shinyApp(ui = ui, server = server)
 rsconnect::deployApp()
